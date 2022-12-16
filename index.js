90ff7abe654d4fe0c7d217c05cad6dc3d303a612
trrez <tatianagutierrezolivares15@gmail.com>
2022-12-15 22:30:25 -0300

    change a bit

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.querySelector("#cards-el");
+let player = {
+  name: "Tatiana",
+  chips: 145,
+};
+let playerEl = document.querySelector("#player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
+
 //otra manera de hacerlo con querySelector = let sumEl = document.querySelector("#sum-el");
 //El query selector puede seleccionar cualquier selector de css, cualquier etiqueta.
 //por ejemplo si es por el selector id tiene que ser con un hashtag(#) al principio, si es
 //con una clase con un punto(.) al principio.
 //si es por ej con el body se debe poner solo body.
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -32,8 +58,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-  let card = 7;
-  sum += card;
-  startGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
